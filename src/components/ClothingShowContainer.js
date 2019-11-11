5242b676c960aa7c6e568898c2e29442be3c5a76
icydwei <robpaik@gmail.com>
2019-11-11 18:38:51 -0500

    formatting clothing show page

--- src/components/ClothingShowContainer.js
+++ src/components/ClothingShowContainer.js
@@ -15,17 +15,30 @@ const ClothingShowContainer = props => {
     <Container fluid>
       <Grid celled>
         <Grid.Row>
-          <Grid.Column width={4}>
+          <Grid.Column width={4} id='clothingShowImgCol'>
+            <Card fluid>
+            <Card.Content textAlign='center'>
             <Image id='clothingShowImg' src={props.clothing.image_url}/>
+            </Card.Content>
+            <Card.Content>
+            <Image
+             floated='right'
+             id="clothingShowUser"
+             src={clothing.user.avatar}
+            />
+            <Card.Header id='clothingShowUserPosted'>posted by</Card.Header>
+            <Card.Meta id='clothingShowUserName'>{clothing.user.username}</Card.Meta>
+            </Card.Content>
+            </Card>
           </Grid.Column>
-          <Grid.Column width={12}>
+          <Grid.Column width={12} id='clothingShowDetailCol'>
             <Card fluid>
               <Card.Content>
                 <Card.Description>
                   <Grid>
                     <Grid.Column floated='left' width={1}>
                       <div>
-                        <Button animated='fade' color='black'
+                        <Button animated='fade' color='black' size='large'
                           as={ Link }
                           to = {`/home`}>
                         <Button.Content visible>Back</Button.Content>
@@ -43,13 +56,7 @@ const ClothingShowContainer = props => {
                 <Card.Header id="clothesShowBrand">{clothing.brand}</Card.Header>
                 <Card.Header id="clothesShowName">{clothing.name}</Card.Header>
 
-                <Image
-                 floated='right'
-                 id="clothingShowUser"
-                 src={clothing.user.avatar}
-                />
-              <Card.Header id='clothingShowUserPosted'>posted by</Card.Header>
-                <Card.Meta id='clothingShowUserName'>{clothing.user.username}</Card.Meta>
+
                 <Card.Description id='clothingShowDescription'>
                   {clothing.description}
                 </Card.Description>
